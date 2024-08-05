b9a833ebeab4656acedd5ef37452da233a0833ba
ryoon-joker <ryankm78@gmail.com>
2024-08-04 19:47:51 -0700

    change 1

---
+++ mockito.example/src/main/java/com/example/LibraryService.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.HashMap;
+
+public class LibraryService {
+
+    HashMap hashMap = new HashMap<User, Book>();
+
+    private void reserveBook(int bookId, int userId){
+        hashMap.put( b, );
+    } 
+
+    private void cancelReservation(int bookId){
+        hashMap.remove(bookId);
+    }
+}
